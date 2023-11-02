bd12bdb525f0ca638c36ed4875e6317173fef54e
Alina Kladieva <alina.kladieva@intel.com>
2023-11-02 22:31:33 +0100

    Add "labeler" tests

--- __tests__/labeler.test.ts
+++ __tests__/labeler.test.ts
@@ -13,31 +13,56 @@ beforeAll(() => {
 const matchConfig = [{any: ['*.txt']}];
 
 describe('checkGlobs', () => {
-  it('returns true when our pattern does match changed files', () => {
-    const changedFiles = ['foo.txt', 'bar.txt'];
-    const result = checkGlobs(changedFiles, matchConfig, false);
+    it('returns true when our pattern does match changed files', () => {
+      const changedFiles = ['foo.txt', 'bar.txt'];
+      const result = checkGlobs(changedFiles, matchConfig, false);
+      
+      expect(result[0]).toBeTruthy();
+    });
+    
+    it('returns false when our pattern does not match changed files', () => {
+      const changedFiles = ['foo.docx'];
+      const result = checkGlobs(changedFiles, matchConfig, false);
+ 
+      expect(result[0]).toBeFalsy();
+    });
 
-    expect(result).toBeTruthy();
-  });
+    it('returns false for a file starting with dot if `dot` option is false', () => {
+      const changedFiles = ['.foo.txt'];
+      const result = checkGlobs(changedFiles, matchConfig, false);
 
-  it('returns false when our pattern does not match changed files', () => {
-    const changedFiles = ['foo.docx'];
-    const result = checkGlobs(changedFiles, matchConfig, false);
+      expect(result[0]).toBeFalsy();
+    });
 
-    expect(result).toBeFalsy();
-  });
+    it('returns true for a file starting with dot if `dot` option is true', () => {
+      const changedFiles = ['.foo.txt'];
+      const result = checkGlobs(changedFiles, matchConfig, true);
 
-  it('returns false for a file starting with dot if `dot` option is false', () => {
-    const changedFiles = ['.foo.txt'];
-    const result = checkGlobs(changedFiles, matchConfig, false);
+      expect(result[0]).toBeTruthy();
+    });
 
-    expect(result).toBeFalsy();
-  });
+    it('non matching files are correct when no match', () => {
+      const changedFiles = ['file.pdf'];
+      const result = checkGlobs(changedFiles, matchConfig, false);
 
-  it('returns true for a file starting with dot if `dot` option is true', () => {
-    const changedFiles = ['.foo.txt'];
-    const result = checkGlobs(changedFiles, matchConfig, true);
+      expect(result[1]).toEqual(changedFiles);
+    });
 
-    expect(result).toBeTruthy();
-  });
+    it('non matching files are empty when everything matched', () => {
+      const changedFiles = ['file.txt'];
+      const result = checkGlobs(changedFiles, matchConfig, false);
+
+      expect(result[1]).toEqual([]);
+    });
+
+    it('non matching files include all valid values', () => {
+      const config = [
+        {any: ['*.txt', '!file.txt']},
+        {any: ['folder/**/*', '!folder/**/file']}
+      ];
+      const changedFiles = ['folder/subfolder/file', 'folder/subfolder/otherfile', 'file.txt', 'file.pdf'];
+      const result = checkGlobs(changedFiles, config, false);
+
+      expect(result[1].sort()).toEqual(['file.txt', 'file.pdf', 'folder/subfolder/file'].sort());
+    });
 });
